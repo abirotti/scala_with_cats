2fceadb3f6b2acd4181b3687ff8edf97e0277a31
Andrea Birotti <andrea.birotti@gmail.com>
2018-07-06 11:03:12 +0100

    add list of optional integer with Monoid

--- src/main/scala/O2_Monoids_And_Semigroups/syntax/SuperAdder.scala
+++ src/main/scala/O2_Monoids_And_Semigroups/syntax/SuperAdder.scala
@@ -2,12 +2,13 @@ package O2_Monoids_And_Semigroups.syntax
 
 import cats.Monoid
 import cats.instances.int._
+import cats.instances.option._
 import cats.syntax.semigroup._
 
 object SuperAdder {
 
-  def add(items: List[Int]): Int = items.foldRight(Monoid[Int].empty) {
-    (i, acc) =>
+  def add(items: List[Option[Int]]): Option[Int] =
+    items.foldRight(Monoid[Option[Int]].empty) { (i, acc) =>
       acc |+| i
-  }
+    }
 }
--- src/test/scala/O2_Monoids_And_Semigroups/syntax/SuperAdderTest.scala
+++ src/test/scala/O2_Monoids_And_Semigroups/syntax/SuperAdderTest.scala
@@ -2,21 +2,22 @@ package O2_Monoids_And_Semigroups.syntax
 
 import cats.Monoid
 import cats.instances.int._
+import cats.instances.option._
 import org.scalatest.{FreeSpec, Matchers}
 
 class SuperAdderTest extends FreeSpec with Matchers{
 
-  import O2_Monoids_And_Semigroups.syntax.SuperAdder._
+  import SuperAdder._
 
   "Add" - {
     "should add an empty list" in {
-      add(List()) should be(Monoid[Int].empty)
+      add(List()) should be(Monoid[Option[Int]].empty)
     }
     "should add a 1 item list" in {
-      add(List(13)) should be(13)
+      add(List(13).map(Some(_))) should be(Some(13))
     }
     "should add a list" in {
-      add(List(1,2,3,4)) should be(10)
+      add(List(1,2,3,4).map(Some(_))) should be(Some(10))
     }
   }
 
